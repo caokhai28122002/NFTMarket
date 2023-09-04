be520da80a59b34ce6d65aad84166ffa835ce5a4
phamxuanhienkt <phamxuanhientk2002@gmail.com>
2023-09-04 17:14:01 +0700

    feat slide

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -1,18 +1,20 @@
-import MainLayout from '@/layout/MainLayout'
-import { NextPageWithLayout } from '@/types'
-import HeroSection from './components/HeroSection'
-import OurService from './components/OurService'
+import MainLayout from "@/layout/MainLayout";
+import { NextPageWithLayout } from "@/types";
+import HeroSection from "./components/HeroSection";
+import OurService from "./components/OurService";
+import SlideSection from "./components/SlideSection";
 
 const HomePage: NextPageWithLayout = () => {
   return (
     <div>
-      <HeroSection/>
-      <OurService/>
-    </div >
-  )
-}
-export default HomePage
+      <HeroSection />
+      <OurService />
+      <SlideSection />
+    </div>
+  );
+};
+export default HomePage;
 
 HomePage.getLayout = function (page) {
-  return <MainLayout>{page}</MainLayout>
-}
+  return <MainLayout>{page}</MainLayout>;
+};
