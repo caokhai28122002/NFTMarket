1e7f7f792dd4c8b33c269d3b4f1e52313e8564c4
Lê Hữu Hoàn <hoanle396@gmail.com>
2023-09-06 09:35:09 +0700

    add lib qr code

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -3,8 +3,16 @@ import { NextPageWithLayout } from "@/types";
 import HeroSection from "./components/HeroSection";
 import OurService from "./components/OurService";
 import SlideSection from "./components/SlideSection";
-
+import { VietQR } from "@/libraries/qr-code";
 const HomePage: NextPageWithLayout = () => {
+  const vietQR = new VietQR();
+  const value = vietQR
+    .setBeneficiaryOrganization("970415", "109871427613")
+    .setTransactionAmount("50000")
+    .setAdditionalDataFieldTemplate("noidunggiaodich")
+    .build();
+  console.log({ value });
+
   return (
     <div>
       <HeroSection />
