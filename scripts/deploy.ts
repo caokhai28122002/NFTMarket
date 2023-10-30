821f1f2406059973c229f110882743507bb03a49
Lê Hữu Hoàn <hoanle396@gmail.com>
2023-10-30 19:54:05 +0700

    update script deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,26 +1,17 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const NFTMarket = await ethers.deployContract("NFTMarket");
+  await NFTMarket.waitForDeployment();
 
-  const lockedAmount = ethers.parseEther("0.001");
+  console.log(`NFT Market deployed to ${NFTMarket.target}`);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+  const NFT = await ethers.deployContract("NFT", [NFTMarket.target]);
+  await NFT.waitForDeployment();
 
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`NFT deployed to ${NFT.target}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
