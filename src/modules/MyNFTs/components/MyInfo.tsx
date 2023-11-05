3fd9d1ee244b4a3dd0c6e24a58fcf08448f80f81
phamxuanhienkt <phamxuanhientk2002@gmail.com>
2023-11-05 14:59:33 +0700

    fix acount

--- src/modules/MyNFTs/components/MyInfo.tsx
+++ src/modules/MyNFTs/components/MyInfo.tsx
@@ -1,20 +1,21 @@
 import Avatar from "@/components/Avatar";
+import useAccount from "@/hooks/useAccount";
 import { cutString } from "@/libraries/utils";
 import { FCC } from "@/types";
 import React from "react";
 
 const MyInfo: FCC = () => {
+  const { account } = useAccount();
+
   return (
     <div className="flex flex-row items-center gap-3 px-4 py-8 w-full ">
       <Avatar
-        username={"0x8435068Abdf9f9003c9CFb790F5446b9C4C0f3A6"}
+        username={account ?? ""}
         className="w-40 h-40 border rounded-3xl"
       />
       <div className="flex flex-col w-full">
         <a className="text-[#8A8AA0] text-sm">Owned By</a>
-        <a className="text-3xl text-white">
-          {cutString("0x8435068Abdf9f9003c9CFb790F5446b9C4C0f3A6", 10, 7)}
-        </a>
+        <a className="text-3xl text-white">{cutString(account ?? "", 10, 7)}</a>
       </div>
     </div>
   );
