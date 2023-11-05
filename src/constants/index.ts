9e1270ddbb2ff581e89767ecba4ae3c8f257e116
phamxuanhienkt <phamxuanhientk2002@gmail.com>
2023-11-05 15:20:06 +0700

    deploy contract

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const NFTMarketAddress = "0x4dC50524bCd5EBAc0bF2343AfEaBB330Ba3FC1e9";
+export const NFTMarketAddress = "0xcD18E762166a0beBF78096A9B12c4C4c0B93123F";
 
 export const PINATA_IPFS_URL= process.env.NEXT_PUBLIC_PINATA_IPFS_URL as string;
 export const PINATA_API_KEY = process.env.NEXT_PUBLIC_PINATA_API_KEY as string;
